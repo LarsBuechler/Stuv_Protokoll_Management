8381ee6668d086387a2eb8dd06c173fba1d9fc9b
LarsBuechler <larsbuechler@web.de>
2024-05-24 00:55:39 +0200

    Random Video Player

--- frontend/stuv_project/app/LeweFan/page.tsx
+++ frontend/stuv_project/app/LeweFan/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout/layout'
+import Video from 'next-video'
+import Nackt_und_wild from '@/videos/Nackt_und_wild.mp4'
 
 const page = () => {
   return (
-    <div className='text-center text-5xl text-primary'>Lewe is Geil!</div>
+    
+    <Layout>
+      <div className='text-center text-5xl text-primary pt-20 pr-10'>Lewe is awesome: hear for yourself!</div>
+      <Video src={Nackt_und_wild} style={{maxWidth: '42rem'} } />
+    </Layout>
   )
 }
 
